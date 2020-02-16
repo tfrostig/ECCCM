e9ca3a3562a532fe72ea2fc61d834cdcaf1e88f4
Tzviel Frostig <tfrostig@gmail.com>
2020-02-16 11:14:02 +0200

    Removed old version of variance estimation.

--- R/Variance_functions.R
+++ R/Variance_functions.R
@@ -4,50 +4,6 @@ findOmegaBeta <- function(beta.mc, omega, sigma, n.o) {
                              'omega_beta_var' = (sigma^2 / n.o) * diag(omega %*% omega %*% omega))
 }
 
-### Old version of matrix estimation
-estimateVariance <- function(g.r, sig.r, inv.r, beta.joint.est, n.o, method = 'moment') {
-  n.r   <- nrow(g.r)
-  p     <- ncol(g.r)
-  if (method == 'moment') {
-    if (ncol(g.r) == 1) {
-      sig.est.vec     <- apply(g.r, 1, function(x) as.vector(x %*% t(x))) ### Strange behaviour of apply with 1 dimension
-      A.r             <- drop((t(sig.est.vec) %*% sig.est.vec / n.r) - sig.r * sig.r)
-      kron.beta.inv.r <- beta.joint.est * drop(inv.r)
-      term.2.var.r    <- A.r * t(kron.beta.inv.r) %*% kron.beta.inv.r / n.r
-      ## Term 3
-      beta.inv.r      <- drop(t(beta.joint.est) * drop(inv.r))
-      term.3.var.r    <- A.r * t(beta.inv.r) %*% beta.inv.r / n.o
-    }
-    if (ncol(g.r) > 1) {
-      ### Variance of covariance matrix
-      outer.prod    <- outerProdRow(g.r)
-      A.r           <- findCovVar(outer.prod, cov(g.r))
-      ## Term 2
-      kron.beta.inv.r <- kronecker(t(beta.joint.est), inv.r)
-      term.2.var.r    <- kron.beta.inv.r %*% A.r %*% t(kron.beta.inv.r) / n.r
-      ## Term 3
-      beta.inv.r           <- t(beta.joint.est) %*% inv.r
-      kron.beta.inv.diag.r <- kronecker(beta.inv.r, diag(p))
-      term.3.var.r         <- kron.beta.inv.diag.r %*% A.r %*% t(kron.beta.inv.diag.r) / n.o
-    }
-  }
-  if (method == 'normal') {
-    c1.r          <- ((n.r - p) * (n.r - p - 1) * (n.r - p - 3))^(-1)
-    d1.r          <- ((n.r - p) * (n.r - p - 1)^2 * (n.r - p - 3))^(-1)
-    ## Term 2
-    term.2.var.r  <- n.r^2 *  (c1.ref * drop(t(beta.joint.est) %*% sig.r %*% beta.joint.est) * inv.r +
-                                 (c1.r + 2 * d1.r) * beta.joint.est %*% t(beta.joint.est))
-    ## Term 3
-    term.3.var.r  <- (drop(t(beta.joint.est) %*% inv.r %*% beta.joint.est) *
-                        sig.r + beta.joint.est %*% t(beta.joint.est)) / n.o
-  }
-  return(list('var.term.2' = term.2.var.r,
-              'var.term.3' = term.3.var.r,
-              'total'      = (term.2.var.r) + (term.3.var.r)))
-}
-
-
-
 
 #' estimateVarAdd - estimation of additional variance
 #' @param beta.mc The estimate of coefficient of joint regression (transformed marginal beta)
